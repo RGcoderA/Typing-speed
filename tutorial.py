92781405c9e9118f3b31ea161397d72377b1604d
Rohil Gugunta <108376638+RGcoderA@users.noreply.github.com>
2023-07-18 15:52:44 -0400

    Add files via upload

---
+++ tutorial.py
@@ -0,0 +1,44 @@
+import curses
+from curses import wrapper
+import time
+import random
+
+
+def start_screen(stdscr):
+	stdscr.clear()
+	stdscr.addstr("Welcome to the Speed Typing Test!")
+	stdscr.addstr("\nPress any key to begin!")
+	stdscr.refresh()
+	stdscr.getkey()
+
+def display_text(stdscr, target, current, wpm=0):
+	stdscr.addstr(target)
+	stdscr.addstr(1, 0, f"WPM: {wpm}")
+
+	for i, char in enumerate(current):
+		correct_char = target[i]
+		color = curses.color_pair(1)
+		if char != correct_char:
+			color = curses.color_pair(2)
+
+		stdscr.addstr(0, i, char, color)
+
+def load_text():
+	with open("text.txt", "r") as f:
+		lines = f.readlines()
+		return random.choice(lines).strip()
+
+def wpm_test(stdscr):
+	target_text = load_text()
+	current_text = []
+	wpm = 0
+	start_time = time.time()
+	stdscr.nodelay(True)
+
+	while True:
+		time_elapsed = max(time.time() - start_time, 1)
+		wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
+
+		stdscr.clear()
+		display_text(stdscr, target_text, current_text, wpm)
+		stdscr.refresh()
