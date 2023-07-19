5b28865c7ddd06b033bb74a4551f893bf61fc2ba
Rohil Gugunta <108376638+RGcoderA@users.noreply.github.com>
2023-07-19 15:55:12 -0400

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -42,3 +42,39 @@ def wpm_test(stdscr):
 		stdscr.clear()
 		display_text(stdscr, target_text, current_text, wpm)
 		stdscr.refresh()
+
+	if "".join(current_text) == target_text:
+			stdscr.nodelay(False)
+			break
+
+		try:
+			key = stdscr.getkey()
+		except:
+			continue
+
+		if ord(key) == 27:
+			break
+
+		if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+			if len(current_text) > 0:
+				current_text.pop()
+		elif len(current_text) < len(target_text):
+			current_text.append(key)
+
+
+def main(stdscr):
+	curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+	curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+	curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+	start_screen(stdscr)
+	while True:
+		wpm_test(stdscr)
+		stdscr.addstr(2, 0, "You completed the text! Press any key to continue...")
+		key = stdscr.getkey()
+
+		if ord(key) == 27:
+			break
+
+
+wrapper(main)
